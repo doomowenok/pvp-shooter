1807c381b54b72606a2955d0afff72171d346ac6
doomowenok <doomowenok@icloud.com>
2025-09-30 16:42:01 +0300

    Initial commit, add external content, configure gitignore.

---
+++ Source/ShootThemUp/Public/STUGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "STUGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTTHEMUP_API ASTUGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
